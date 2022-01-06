3b336c76931299305ff43cb71e3d4d7aa0728e3d
Flamcom27 <27flamcom06@gmail.com>
2022-01-06 23:53:29 +0300

    first commit

---
+++ ASP.NET  core tutorial project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET__core_tutorial_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
